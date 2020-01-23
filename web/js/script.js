61277fc4f0b1da03d9aadf1d7a01ffb74104bbe3
rnumata <regis.numata@gmail.com>
2020-01-23 09:12:28 -0300

    Add files via upload

---
+++ web/js/script.js
@@ -0,0 +1,13 @@
+
+if (window.SimpleSlide){
+    new SimpleSlide({
+        slide: "sobre",
+        auto: true,
+        time: 10000
+    })
+}
+
+
+if (window.SimpleAnime){
+    new SimpleAnime();
+}
